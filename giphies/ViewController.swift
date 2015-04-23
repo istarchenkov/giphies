045539c27dba8c9e1d9940d4a283589516624654
istarchenkov <istarchenkov@gmail.com>
2015-04-23 23:07:11 +0300

    Initial Commit

---
+++ giphies/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  giphies
+//
+//  Created by Ivan Starchenkov on 23/04/15.
+//  Copyright (c) 2015 Ivan Starchenkov. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
